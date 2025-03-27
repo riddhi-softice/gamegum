2e54f476cd5c172df53e6fd5d348776d642affdf
Riddhi <riddhisoftice@gmail.com>
2025-03-27 18:01:10 +0530

    after live website changes

--- game-subdomain/Assert/js/display_landing.js
+++ game-subdomain/Assert/js/display_landing.js
@@ -70,20 +70,20 @@ window.addEventListener('popstate', (event) => {
 });
 createPopupAd();
 
-// PAGE RIGHT CLICK DISABLE
-document.addEventListener("contextmenu", function (event) {
-    event.preventDefault();
-	// alert("Right-click is disabled!");
-});
+// // PAGE RIGHT CLICK DISABLE
+// document.addEventListener("contextmenu", function (event) {
+//     event.preventDefault();
+// 	// alert("Right-click is disabled!");
+// });
 
-// VIEWING SOURCE DISABLE
-document.addEventListener("keydown", function (event) {
-    if (event.ctrlKey && (event.key === "u" || event.key === "U")) {
-        event.preventDefault();
-        // alert("Viewing source is disabled!");
-    }
-    if (event.key === "F12" || (event.ctrlKey && event.shiftKey && (event.key === "i" || event.key === "j" || event.key === "I" || event.key === "J"))) {
-        event.preventDefault();
-        // alert("Inspect Element is disabled!");
-    }
-});
+// // VIEWING SOURCE DISABLE
+// document.addEventListener("keydown", function (event) {
+//     if (event.ctrlKey && (event.key === "u" || event.key === "U")) {
+//         event.preventDefault();
+//         // alert("Viewing source is disabled!");
+//     }
+//     if (event.key === "F12" || (event.ctrlKey && event.shiftKey && (event.key === "i" || event.key === "j" || event.key === "I" || event.key === "J"))) {
+//         event.preventDefault();
+//         // alert("Inspect Element is disabled!");
+//     }
+// });
