fc7bca57e5b9f6e331e4693bbffd70001b13d801
Riddhi <riddhisoftice@gmail.com>
2025-04-03 17:57:51 +0530

    new adx add

---
+++ Live/games/assets/js/group1_auto_landing.js
@@ -0,0 +1,13 @@
+// Auto Ads
+(function() {
+    const script = document.createElement('script');
+    script.async = true;
+    script.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8263720079379422";
+    script.crossOrigin = "anonymous";
+
+    document.head.appendChild(script);
+})();
+     
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelector(".top-adx").style.setProperty("height", "100px", "important");
+});
