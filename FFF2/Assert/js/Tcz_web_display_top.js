c9ca2d37456f7cee0eeb62b9cb4282c7088c9e3d
Riddhi <riddhisoftice@gmail.com>
2025-04-29 17:43:29 +0530

    fff2 live

--- FFF2/Assert/js/Tcz_web_display_top.js
+++
@@ -1,9 +0,0 @@
-console.log(typeof page !== "undefined" ? page : 'default', "page");
-window.googletag = window.googletag || { cmd: [] };
-googletag.cmd.push(function () {
-  googletag.defineSlot('/23201071713/web_display', [[300, 250], [300, 600], [320, 480], [336, 280]], 'div-gpt-ad-1741683912871-0').addService(googletag.pubads());
-  googletag.pubads().collapseEmptyDivs();
-  googletag.pubads().setCentering(true);
-  googletag.pubads().setTargeting('p', [typeof page !== "undefined" ? page : 'default']);
-  googletag.enableServices();
-});
